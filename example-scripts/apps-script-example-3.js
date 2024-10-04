a49ee41f386bd17b82c70a564a42864525b69f47
Sean Oberc <sean.oberc@techelevator.com>
2024-10-04 17:41:09 -0400

    delete: example 3.0; rename: example v3.2 to example 3

--- example-scripts/apps-script-example-3.js
+++ example-scripts/apps-script-example-3.js
@@ -1,34 +1,24 @@
-function calSummaryToEmail() {
-    let date = new Date();
-    date.formatDate();
-    let eventsList = CalendarApp.getDefaultCalendar().getEventsForDay(date);
-    let emailMessage = 'These are the events on your calendar for today, ' + date + ':\n';
+/* This function gets the list of the events from your Google Calendar for the current day */
+function sendCalEventsToEmail() {
+    // create a new object of type Date; this will be passed in a method invoked on the CalendarApp service
+    let todaysDate = new Date();
 
+    // creates a list of events from Google Calendar; user's personal Gmail ID is hardcoded
+    let eventsList = CalendarApp.getCalendarById("seanoberc@gmail.com").getEventsForDay(todaysDate);        // .getEventsForDay() get's todaysDate passed in as an argument
+    // the default message is initialized here so that it's in scope for the following if statement
+    let emailMessage = 'These are the events on your calendar for today: \n\n';
+
+    // if there are no events for the day in the list, just print following
     if (eventsList.length === 0) {
         emailMessage = 'You have nothing scheduled today.';
     } else {
+        // inspect each element in eventsList using a for each loop
+        // append the event title and event start time to today's event element in the eventList
         for (let event in eventsList) {
             emailMessage += eventsList[event].getTitle() + '; starts at: ' + eventsList[event].getStartTime() + '\n';
         }
     }
 
-    MailApp.sendEmail(Session.getActiveUser().getEmail(), 'Today\'s Events', emailMessage);
+    // Finally, 
+    GmailApp.sendEmail(Session.getActiveUser().getEmail(), 'Today\'s Events', emailMessage);
 }
-
-function formatDate() {
-    let date = new Date();
-    
-    // Get day of the week, month, day with suffix, and year
-    let daysArr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    let monthsArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    
-    let dayOfWeek = daysArr[date.getDay()];
-    let month = monthsArr[date.getMonth()];
-    let day = date.getDate();
-    
-    let formattedDate = dayOfWeek + ", " + day + month;
-    
-    Logger.log(formattedDate); // Outputs: Monday, 15 January
-    return formattedDate;
-  }
-  
