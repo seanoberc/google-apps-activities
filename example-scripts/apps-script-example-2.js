3dd2e3ce649c49e6b3f1b1e010816100575fc582
Sean Oberc <sean.oberc@techelevator.com>
2024-10-06 16:04:08 -0400

    rename: example 2 to version 1; create: new example 2

--- example-scripts/apps-script-example-2.js
+++ example-scripts/apps-script-example-2.js
@@ -1,11 +1,44 @@
-/* creates a custom menu in the Sheets UI and adds the insertDate function to that menu */
+/* creates a custom menu in the Google Forms UI */
 function onOpen() {
-  // inovke the .getUi() method on the global object, assign it all to a variable
-  let ui = SpreadsheetApp.getUi();
+  // uses the Forms service to get the UI object and save it to a variable
+  let ui = FormApp.getUi();
 
-  // use the ui object to create a new menu
-  ui.createMenu('Custom Menu')                // passes in a name for the menu
-    .addItem('Insert Date', 'insertDate')     // calls .addItem() method; first argument provides a name for the item & second argument provides our custom insertDate() func
-    .addToUi()                                // method adds this stuff to our custom menu
-  ;
+// invokes method on the UI object to create a new menu named 'custom menu'
+  ui.createMenu('Custom Menu')
+    .addItem('Generate Form Questsions', 'generateCommonQuestions')       // passes in the title of the menu item and the name of the function to be run
+    .addToUi();
+}
+
+/* generates the questions for the form */
+function generateCommonQuestions() {
+  // gets the current active form using the Forms service for Google Script
+  let form = FormApp.getActiveForm();
+
+  /* the following adds items to the form and chains methods to set the name of each item and whether or not particular data are required by the form */
+  form.addTextItem()
+    .setTitle('Name')
+    .setRequired(true);
+
+  form.addTextItem()
+    .setTitle('Email')
+    .setRequired(true);
+
+  // phone number will not be a required field unlike the two above
+  form.addTextItem()
+    .setTitle('Phone Number')
+    .setRequired(false);
+
+  // instantiates a variable to hold a mutliple choice item for the form
+  let multiChoiceItem = form.addMultipleChoiceItem();
+  form.addMultipleChoiceItem()                          // sets the title for this item and then creates the names of the different choices by passing in an array
+    .setTitle('Preferred Contact Method')
+    .setChoices(
+      [
+        multiChoiceItem.createChoice('Email'),
+        multiChoiceItem.createChoice('Text Message')
+      ]
+    ).setRequired(true);
+
+    // alers the person making the form that the questions have been successfully created
+    FormApp.getUi().alert('Common questions have been generated for this form.');
 }
